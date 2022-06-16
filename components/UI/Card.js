aa5b07b569cce4b0ee18948f95c48cf4490af2d2
adamovic6666 <adamovicslobodan@yahoo.com>
2022-06-16 15:50:04 +0200

    removed ref from card component

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -1,17 +1,16 @@
 import classes from "./Card.module.css";
 import Video from "./Video";
-import React from "react";
 
-const Card = React.forwardRef((props, ref) => {
+const Card = (props) => {
   const { mediaUrl, mediaUrlWebm, children } = props;
   return (
-    <div className={classes.Card} ref={ref}>
+    <div className={classes.Card}>
       <div className={classes.CardVideoWrapper}>
         <Video mediaUrlWebm={mediaUrlWebm} mediaUrlMp4={mediaUrl} />
       </div>
       <div className={classes.CardInfoWrapper}>{children}</div>
     </div>
   );
-});
+};
 
 export default Card;
