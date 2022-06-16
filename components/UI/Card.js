32762fcdd04f740f53df6e45ae5340312497b466
adamovic6666 <adamovicslobodan@yahoo.com>
2022-06-16 15:44:33 +0200

    added ref to card component

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -2,9 +2,10 @@ import classes from "./Card.module.css";
 import Video from "./Video";
 import React from "react";
 
-const Card = React.forwardRef(({ mediaUrlWebm, mediaUrl, children }, ref) => {
+const Card = React.forwardRef((props, ref) => {
+  const { mediaUrl, mediaUrlWebm, children } = props;
   return (
-    <div className={classes.Card}>
+    <div className={classes.Card} ref={ref}>
       <div className={classes.CardVideoWrapper}>
         <Video mediaUrlWebm={mediaUrlWebm} mediaUrlMp4={mediaUrl} />
       </div>
